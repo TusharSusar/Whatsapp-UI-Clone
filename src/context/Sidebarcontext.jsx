878840ab9552b7fe941cb774d2aca76c3fe628f6
TusharSusar <tusharsusar06@gmail.com>
2025-07-27 01:41:34 +0530

    Sidebar Created with reducer

---
+++ src/context/Sidebarcontext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useReducer } from "react";
+
+export const SidebarContext = createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHAT":
+      return "CHAT";
+
+    case "STATUS":
+      return "STATUS";
+
+    case "CHANNELS":
+      return "CHANNELS";
+
+    case "COMMUNITY":
+      return "COMMUNITY";
+
+    default:
+      return state;
+  }
+};
+
+export const SidebarProvider = ({ children }) => {
+  const [sidebarItem, dispatch] = useReducer(reducer, "CHAT");
+
+  return (
+    <SidebarContext.Provider value={{ sidebarItem, dispatch }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
